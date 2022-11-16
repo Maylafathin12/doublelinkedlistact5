e1ede31dd4cbc40c37c90ebac2a8bf1863aa9b1c
TA1069 <maylafaat@gmail.com>
2022-11-16 16:46:27 +0700

    add public void  revtraverse

--- doublelinkedlistact5/Program.cs
+++ doublelinkedlistact5/Program.cs
@@ -105,5 +105,23 @@ namespace doublelinkedlistact5
                     Console.Write(currentNode.rollNumber + "" + currentNode.name + "\n");
             }
         }
+        public void revtraverse()
+        {
+            if (listEmpty())
+                Console.WriteLine("List Empty");
+            else
+            {
+                Console.WriteLine("\nRecords in the descending order of" + "roll numbers are:\n");
+                Node currentNode;
+                for (currentNode = START; currentNode != null;
+                    currentNode = currentNode.next)
+                { }
+                while (currentNode != null)
+                {
+                    Console.Write(currentNode.rollNumber + "" + currentNode.name + "\n");
+                    currentNode = currentNode.prev;
+                }
+            }
+        }
     }
 }
