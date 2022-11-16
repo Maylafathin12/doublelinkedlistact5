949ae3ae85f07b17d1330212bb5e21a802215225
TA1069 <maylafaat@gmail.com>
2022-11-16 16:05:06 +0700

    Add project files.

---
+++ doublelinkedlistact5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace doublelinkedlistact5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
